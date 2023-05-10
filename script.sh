f3e267a69cdd472185e7fb3358e9b18d5f6db088
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-05-10 22:25:06 +0600

    Task 01

---
+++ script.sh
@@ -0,0 +1,12 @@
+mkdir projects
+cd projects
+mkdir my_projects
+cd my_projects
+mkdir directory1 directory2 directory3
+mkdir directory1/subdirectory1
+cd directory2
+echo 'Hello from JS' > hello.txt
+cd ..
+touch directory2/hello.txt
+mkdir directory3/subdirectory2
+cd ../../
